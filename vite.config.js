219f340dc93084a3eb3d4cffcb40323d8dde4b3c
Kirill <kvn2001@internet.ru>
2024-09-20 00:41:59 +0300

    Portfolio1

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import inspect from 'vite-plugin-inspect';
 
+
+
 export default defineConfig({
   plugins: [ inspect(), vue()],
+  base: '/PortfolioClearJS/'
 });
+
